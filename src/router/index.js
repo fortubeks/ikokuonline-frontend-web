bbe6e0102644c0b3938c93a7b8a8d080776ee8d1
Fortune Bekee <fortubeks@gmail.com>
2025-08-11 23:57:20 +0100

    .

--- src/router/index.js
+++ src/router/index.js
@@ -16,11 +16,11 @@ import ProductCategoriesForm from '../pages/dashboard/product-categories/Form.vu
 import Orders from '../pages/dashboard/orders/Orders.vue';
 import OrderShow from '../pages/dashboard/orders/Show.vue';
 import ContactUs from '@/pages/contact-us/ContactUs.vue';
-// import SellerDashboardLayout from '@/layouts/SellerDashboardLayout.vue';
-// import DashBoardHome from '@/pages/sellerdashboard/DashBoardHome.vue';
-// import SellerProfile from '@/pages/sellerdashboard/SellerProfile.vue';
-// import CarPart from '@/pages/sellerdashboard/listings/CarPart.vue';
-// import VehiclesList from '@/pages/sellerdashboard/listings/VehiclesList.vue';
+import SellerDashboardLayout from '@/layouts/SellerDashboardLayout.vue';
+import DashBoardHome from '@/pages/sellerdashboard/DashBoardHome.vue';
+import SellerProfile from '@/pages/sellerdashboard/SellerProfile.vue';
+import CarPart from '@/pages/sellerdashboard/listings/CarPart.vue';
+import VehiclesList from '@/pages/sellerdashboard/listings/VehiclesList.vue';
 
 import { useAuthStore } from '@/stores/auth';
 import ShopLayout from '@/layouts/ShopLayout.vue';
@@ -78,7 +78,7 @@ const routes = [
   path: '',
   component: ShopLayout,
   children: [
-  
+
 
     {
       path: '/contact',
@@ -106,27 +106,27 @@ const routes = [
   { path: '/test', name: 'test', component: Test },
 
 
-  // {
-  //   path: '/seller-dashboard',
-  //   component: SellerDashboardLayout,
+  {
+    path: '/seller-dashboard',
+    component: SellerDashboardLayout,
 
-  //   children: [
-  //      {
-  //     path: '',
-  //     name: 'seller-dashboard',
-  //     component: DashBoardHome,
-  //   },
-  //     { path: '/sellerprofile', name: 'seller-profile', component: SellerProfile },
-  //     { path: '/listingscarparts', name: 'carpart', component: CarPart },
-  //     { path: '/listingsvehicleslist', name: 'vehicle list', component: VehiclesList,  },
+    children: [
+       {
+      path: '',
+      name: 'seller-dashboard',
+      component: DashBoardHome,
+    },
+      { path: '/sellerprofile', name: 'seller-profile', component: SellerProfile },
+      { path: '/listingscarparts', name: 'carpart', component: CarPart },
+      { path: '/listingsvehicleslist', name: 'vehicle list', component: VehiclesList,  },
 
 
-  //   ],
-  // },
+    ],
+  },
   {
     path: '/dashboard',
     component: DashboardLayout,
-    meta: { requiresAuth: true },
+    meta: { requiresAuth: false },
     children: [
       {
         path: '',
